1f05dced05b760002a1e6af6a743ab8681cce880
Asier Villar <asiervillar@outlook.es>
2022-03-28 20:54:38 +0200

    Creacion de canales

---
+++ bot/events/client/voiceStateUpdate.js
@@ -0,0 +1,16 @@
+const { Collection } = require('discord.js')
+const voiceCollection = new Collection();
+
+module.exports = async (oldstate, newstate) => {
+    if (!oldstate.channelId && newstate.channelId) {
+        if (newstate.channelId == "958075466047176767") {
+            newstate.guild.channels.create(`Sala de ${newstate.member.user.username}`, {
+                type: "GUILD_VOICE",
+                parent: newstate.channel.parent
+            }).then((channel) => {
+                newstate.member.voice.setChannel(channel)
+                voiceCollection.set(newstate.member.user.id, channel.id)
+            })
+        }
+    }
+}
