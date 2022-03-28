8c86e4260dc3ec7a83bfb37438cb57b7235adc57
Asier Villar <asiervillar@outlook.es>
2022-03-28 20:58:14 +0200

    Cambios en el index

--- bot/index.js
+++ bot/index.js
@@ -3,21 +3,23 @@ const config = require('./settings/config.json');
 require('colors');
 
 const client = new Discord.Client({
+    partials: ["MESSAGE", "CHANNEL", "REACTION"],
     intents: [
         Discord.Intents.FLAGS.GUILDS,
         Discord.Intents.FLAGS.GUILD_MEMBERS,
         Discord.Intents.FLAGS.GUILD_MESSAGES,
+        Discord.Intents.FLAGS.GUILD_VOICE_STATES
     ],
 });
 
 client.commands = new Discord.Collection();
 client.aliases = new Discord.Collection();
 
-function requerirhandlers(){
+function requerirhandlers() {
     ["command", "events"].forEach(handler => {
         try {
             require(`./handlers/${handler}`)(client, Discord)
-        } catch(e){
+        } catch (e) {
             console.warn(e)
         }
     })
