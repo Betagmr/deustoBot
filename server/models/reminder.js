b1e2f5854b2e69213e31690844aa7476e8dff63f
Angel de la Peña <angeldelapena@opendeusto.es>
2022-03-30 16:28:08 +0200

    ADD reminde schema and routes

---
+++ server/models/reminder.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose')
+
+const reminderSchema = new mongoose.Schema({
+    hour: {
+        type: String,
+        required: true,
+    },
+    date: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    userId: {
+        type: String,
+        required: true,
+    }
+})
+
+reminderSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject._id
+        delete returnedObject.__v
+    }
+})
+
+module.exports = mongoose.model('Reminder', reminderSchema)
---
+++ server/routes/reminderRouter.js
@@ -0,0 +1,35 @@
+const reminderRouter = require('express').Router()
+const Reminder = require('../models/reminder')
+
+reminderRouter.get('/', (request, response) => {
+    Reminder.find({}).then(reminder => {
+        response.json(reminder)
+    })
+})
+
+reminderRouter.post('/', (request, response, next) => {
+    const body = request.body
+
+    const reminder = new Reminder({
+        hour: body.hour,
+        date: body.date,
+        description: body.description,
+        userId: body.userId
+    })
+
+    reminder.save()
+        .then(savedReminder => {
+            response.json(savedReminder)
+        })
+        .catch(error => next(error))
+})
+
+reminderRouter.delete('/:id', (request, response, next) => {
+    Reminder.findByIdAndRemove(request.params.id)
+        .then(() => {
+            response.status(204).end()
+        })
+        .catch(error => next(error))
+})
+
+module.exports = reminderRouter
