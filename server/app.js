22abb9e3d1673a6dc27e562c1bf4714ce28ab58c
Angel de la Peña <angeldelapena@opendeusto.es>
2022-03-30 16:29:43 +0200

    ADD coin and reminder apis

--- server/app.js
+++ server/app.js
@@ -1,23 +1,29 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+
 const config = require("./utils/config");
 const logger = require("./utils/logger");
 const middleware = require("./utils/middleware");
 const mongoose = require("mongoose");
-const blogRouter = require("./routes/blogcontroller");
+
+const reminderRouter = require("./routes/reminderRouter");
+const coinRouter = require("./routes/coinRouter");
 
 // Database conection
 mongoose
-  .connect(config.MONGODB_URI)
-  .then(() => logger.info("connected to MongoDB"))
-  .catch((err) => logger.error("Error conecting to MongoDB: " + err.message));
+    .connect(config.MONGODB_URI)
+    .then(() => logger.info("Connected to MongoDB"))
+    .catch((err) => logger.error("Error conecting to MongoDB: " + err.message));
 
 app.use(cors());
 app.use(express.static("build"));
 app.use(express.json());
 app.use(middleware.requestLogger);
-app.use("/api", blogRouter);
+
+app.use("/api/reminder", reminderRouter);
+app.use("/api/coin", coinRouter);
+
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
 
