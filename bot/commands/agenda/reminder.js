ad525d8021b8c1fc07a91f27cab44f41df52f569
Eva del Hoyo <eva.delhoyo@opendeusto.es>
2022-03-30 12:48:41 +0200

    reminder refactor

--- bot/commands/agenda/reminder.js
+++ bot/commands/agenda/reminder.js
@@ -1,16 +1,24 @@
+const reminderService = require(`${process.cwd()}\\services\\agendaService.js`)
+
 module.exports = {
     name: "reminder",
     aliases: ["recordatorio", "rem", "rec"],
     desc: "Sirve para crear un recordatorio (Hora (HH:MM), dia, descripcion)",
-    run: (client, message, args) => {
+   
+    run: async (client, message, args) => {
         if(args.length < 3) return message.reply("faltan argumentos")
         const cont = args.slice(2, args.length).join(" ")
-        message.reply(`Se te será recordado a las **${args[0]}** del día **${args[1]}** que **${cont}**`)
+        
+        
 
         const objeto = {
             hour: args[0],
-            day: args[1],
-            description: cont
+            date: args[1],
+            description: cont,
+            userId: message.author.id
         }
+        // await reminderService.postReminder(objeto)
+
+        message.reply(`Se te será recordado a las **${args[0]}** del día **${args[1]}** que **${cont}**`)
     }
 }
