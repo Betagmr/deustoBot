3b68f116b4f1129b696c249ad8f75acdb204e883
Angel de la Peña <angeldelapena@opendeusto.es>
2022-04-04 21:04:49 +0200

    Modificadas la gestion de las rutas de la API

--- server/app.js
+++ server/app.js
@@ -7,6 +7,7 @@ const logger = require("./utils/logger");
 const middleware = require("./utils/middleware");
 const mongoose = require("mongoose");
 
+const helpRouter = require("./routes/helpRouter");
 const reminderRouter = require("./routes/reminderRouter");
 const coinRouter = require("./routes/coinRouter");
 
@@ -21,8 +22,9 @@ app.use(express.static("build"));
 app.use(express.json());
 app.use(middleware.requestLogger);
 
-app.use("/api/reminder", reminderRouter);
-app.use("/api/coins", coinRouter);
+app.use("/", helpRouter);
+app.use(config.ROUTES.remainder_url, reminderRouter);
+app.use(config.ROUTES.coin_url, coinRouter);
 
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
---
+++ server/routes/helpRouter.js
@@ -0,0 +1,8 @@
+const helpRouter = require('express').Router()
+const config = require("../utils/config");
+
+helpRouter.get('/', (request, response) => {
+    response.json(config.ROUTES)
+})
+
+module.exports = helpRouter
