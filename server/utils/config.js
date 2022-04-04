379e533aeca896432bd3d498410789fff5edc0db
Angel de la Peña <angeldelapena@opendeusto.es>
2022-04-04 21:04:31 +0200

    MODIFY config.js

--- server/utils/config.js
+++ server/utils/config.js
@@ -1,9 +1,18 @@
 require("dotenv").config();
 
 const PORT = process.env.PORT;
-const MONGODB_URI = process.env.MONGODB_URI;
+
+const MONGODB_URI = process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'test'
+? process.env.TEST_MONGODB_URI 
+: process.env.MONGODB_URI
+
+const ROUTES = {
+    remainder_url: "/api/reminder",
+    coin_url: "/api/coin"
+}
 
 module.exports = {
   MONGODB_URI,
   PORT,
+  ROUTES,
 };
