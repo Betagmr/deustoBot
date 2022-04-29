60a3d7df30a3ea6a6bf7b54b20f23ea3f25dc7d4
Angel de la Peña <angeldelapena@opendeusto.es>
2022-04-29 13:09:38 +0200

    Comando wallet

---
+++ bot/commands/gambling/wallet.js
@@ -0,0 +1,22 @@
+const { Client, Message } = require('discord.js');
+const game = require('../../handlers/game');
+const gamblingService = require('../../services/gamblingService')
+
+module.exports = {
+    name: "wallet",
+    aliases: ["wall"],
+    desc: "Ver cuantas monedas tiewnes para jugar",
+
+    /**
+     * @param { Client } client
+     * @param { Message } message
+     * 
+    */
+    run: async (client, message, args) => {
+        const userId = message.author.id;
+        await game.ensureUser(userId);
+
+        const userData = await gamblingService.getGamblingPlayer(userId);
+        message.reply(`Tienes ${userData.coins} monedas para apostar.`);
+    }
+};
