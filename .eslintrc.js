27ffa3e812b32e4678b97cd65e613d457dd32aa4
Angel de la Peña <angeldelapena@opendeusto.es>
2022-04-29 18:58:51 +0200

    Reubicado y cambiados ignorados

---
+++ .eslintrc.js
@@ -0,0 +1,40 @@
+module.exports = {
+  'env': {
+    'commonjs': true,
+    'es2021': true,
+    'node': true
+  },
+  'extends': 'eslint:recommended',
+  'parserOptions': {
+    'ecmaVersion': 'latest'
+  },
+  'ignorePatterns': ['/bot/jest.config.js', '/server/jest.config.js', '/bot/test/*', '/server/test/*'],
+  'rules': {
+    'indent': [
+      'error',
+      2
+    ],
+    'linebreak-style': [
+      'error',
+      'unix'
+    ],
+    'quotes': [
+      'error',
+      'single'
+    ],
+    'semi': [
+      'error',
+      'always'
+    ],
+    'eqeqeq': 'error',
+    'no-trailing-spaces': 'error',
+    'object-curly-spacing': [
+      'error', 'always'
+    ],
+    'arrow-spacing': [
+      'error', { 'before': true, 'after': true }
+    ],
+    'no-console': 0,
+    'no-unused-vars': 'off'
+  }
+};
