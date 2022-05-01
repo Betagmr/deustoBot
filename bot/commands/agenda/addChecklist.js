c2ce7552dadfbd4bb0c80459ca0f641c9606b409
Eva del Hoyo <eva.delhoyo@opendeusto.es>
2022-05-01 16:22:24 +0200

    addChecklist comando y test

---
+++ bot/commands/agenda/addChecklist.js
@@ -0,0 +1,25 @@
+const reminderService = require('../../services/agendaService.js');
+
+module.exports = {
+  name: 'addChecklist',
+  aliases: ['addCheck', 'checklist', 'addch'],
+  desc: 'Sirve para añadir un elemento a una checklist',
+
+  run: async (client, message, args) => {
+    if (args.length < 2) return message.reply('faltan argumentos');
+    const elemento = args.slice(1, args.length).join(' ');
+    console.log(elemento);
+    console.log(args[0]);
+
+    const checklist = {
+      listName: args[0],
+      userId: message.author.id,
+      isCheck: false,
+      content: elemento
+    };
+
+    await reminderService.postChecklist(checklist);
+
+    message.reply('checklist creada!');
+  }
+};
