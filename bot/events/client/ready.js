ca79f1eeae0145450876c04048fcd9a7291d2fff
Eva del Hoyo <eva.delhoyo@opendeusto.es>
2022-05-16 23:49:00 +0200

    evento recordatorio

--- bot/events/client/ready.js
+++ bot/events/client/ready.js
@@ -1,6 +1,22 @@
 const cronUtil = require('../../handlers/cron');
+const reminderService = require('../../services/agendaservice');
 
 module.exports = client => {
   console.log(`Sesion iniciada en ${client.user.tag}`.green);
   cronUtil.resetRewardJob.start();
+  setInterval(async () => {
+    const date = new Date(); // today
+    if (date.getHours() === 8 && date.getMinutes() === 0) {
+    // Aqui haces la llamada y metes el message embed
+      const reminders = await reminderService.getUpcomingReminders();
+      const guild = client.guilds.cache.get('958110264014807090');
+
+      reminders.forEach(async (rem) => {
+        const member = await guild.members.fetch(rem.userId);
+        console.log(member);
+        member.send(rem.description);
+      });
+
+    }
+  }, 59000);
 };
