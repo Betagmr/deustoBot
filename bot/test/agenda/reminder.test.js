05efaef94ea97b1602895dbddabff2023817b921
Diego Miquelez de Mendiluce <diego26ftw@gmail.com>
2022-05-21 21:52:15 +0200

    currencyInfoTest y reminderTest refactor.

--- bot/test/agenda/reminder.test.js
+++ bot/test/agenda/reminder.test.js
@@ -5,14 +5,6 @@ describe('Test del reminder', () => {
     reply: jest.fn()
   };
 
-  // test("Se llama correctamente", () => {
-  //     const args = ["10:20", "10/12/2022", "hola"];
-  //     const expectedOutput = `Se te será recordado a las **10:20** del día **10/12/2022** que **hola**`;
-  //     reminder.run(null, messageMock, args);
-  //     expect(messageMock.reply).toHaveBeenCalledTimes(1);
-  //     expect(messageMock.reply).toHaveBeenCalledWith(expectedOutput);
-  // })
-
   test('Faltan argumentos', () => {
     const args = ['10:20', '10/12/2022'];
     const expectedOutput = 'faltan argumentos';
--- bot/test/finance/currencyInfo.test.js
+++ bot/test/finance/currencyInfo.test.js
@@ -1,15 +1,48 @@
 const currencyInfo = require('../../commands/finance/currencyInfo.js');
+const coinservices = require('../../services/coinservices.js');
+const coinTemplate = require('../../templates/coinTemplate.js');
+const axios = require('axios');
+jest.mock('../../services/coinservices.js')
+jest.mock('../../templates/coinTemplate.js')
+jest.mock('axios');
 
 describe('Test del currencyInfo', () => {
   const messageMock = {
-    reply: jest.fn()
+    reply: jest.fn(),
+    channel: {
+      send: jest.fn()
+    }
   };
+
+  const getCriptoMock = (cripto) => coinservices.getCriptoCurrency.mockResolvedValueOnce(cripto);
 
   test('Faltan argumentos', () => {
     const args = [];
     const expectedOutput = 'No has puesto ninguna criptomoneda.';
+
     currencyInfo.run(null, messageMock, args);
     expect(messageMock.reply).toHaveBeenCalledTimes(1);
     expect(messageMock.reply).toHaveBeenCalledWith(expectedOutput);
+  });
+
+  test('La criptomoneda no ha sido encontrada', async () => {
+    const args = ['asdasdasda'];
+    const expectedOutput = 'No se ha encontrado tu criptomoneda.';
+    getCriptoMock();
+
+    await currencyInfo.run(null, messageMock, args);
+    expect(messageMock.reply).toHaveBeenCalledTimes(1);
+    expect(messageMock.reply).toHaveBeenCalledWith(expectedOutput);
+  });
+
+  test('Criptomoneda encontrada', async () => {
+    const args = ['bitcoin'];
+    const currency = args[0];
+    const coin = getCriptoMock(currency);
+    const expectedOutput = { embeds: [coinTemplate(coin)] };
+
+    await currencyInfo.run(null, messageMock, args);
+    expect(messageMock.channel.send).toHaveBeenCalledTimes(1);
+    expect(messageMock.channel.send).toHaveBeenCalledWith(expectedOutput);
   });
 });
